aa78b505d02c6e39fcb8d851ad2f8e27966cce44
Kyle Snow <k1snow@bridgew.edu>
2020-11-02 12:56:56 -0500

    Initial commit

---
+++ procedural.go
@@ -0,0 +1,26 @@
+package main
+import (
+	// "fmt"
+	"time"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/dji/tello"
+)
+func orig() {
+	drone := tello.NewDriver("8888")
+	work := func() {
+		drone.TakeOff()
+		gobot.After(3*time.Second, func() {
+			drone.Left(50)
+			time.Sleep(time.Second*3)
+			drone.Right(50)
+			time.Sleep(time.Second*3)
+			drone.Land()
+		})
+	}
+	robot := gobot.NewRobot("tello",
+		[]gobot.Connection{},
+		[]gobot.Device{drone},
+		work,
+	)
+	robot.Start()
+}
