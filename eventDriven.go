44884078e9ad7bed4f73b49fb0c0fcea8109c29f
kappafalcon <michaeldrolet272@gmail.com>
2020-11-09 11:29:30 -0500

    Updated to Multiple Threads

--- eventDriven.go
+++ eventDriven.go
@@ -1,35 +1,18 @@
 package main
+
 import (
 	"fmt"
-	"os/exec"
-	"time"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/platforms/dji/tello"
+	"os/exec"
+	"time"
 )
+
 func main() {
 	drone := tello.NewDriver("8890")
 	work := func() {
-		mplayer := exec.Command("mplayer", "-fps", "25", "-")
-		mplayerIn, _ := mplayer.StdinPipe()
-		if err := mplayer.Start(); err != nil {
-			fmt.Println(err)
-			return
-		}
-		drone.On(tello.ConnectedEvent, func(data interface{}) {
-			fmt.Println("Connected")
-			drone.StartVideo()
-			drone.SetVideoEncoderRate(4)
-			gobot.Every(100*time.Millisecond, func() {
-				drone.StartVideo()
-			})
-		})
-		drone.On(tello.VideoFrameEvent, func(data interface{}) {
-			pkt := data.([]byte)
-			if _, err := mplayerIn.Write(pkt); err != nil {
-				fmt.Println(err)
-			}
-		})
-
+		go startCamera(drone)
+		go fly(drone)
 	}
 	robot := gobot.NewRobot("tello",
 		[]gobot.Connection{},
@@ -37,4 +20,33 @@ func main() {
 		work,
 	)
 	robot.Start()
+}
+
+func startCamera(drone *tello.Driver) {
+	mplayer := exec.Command("mplayer", "-fps", "25", "-")
+	mplayerIn, _ := mplayer.StdinPipe()
+	if err := mplayer.Start(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	drone.On(tello.ConnectedEvent, func(data interface{}) {
+		fmt.Println("Connected")
+		drone.StartVideo()
+		drone.SetVideoEncoderRate(4)
+		gobot.Every(100*time.Millisecond, func() {
+			drone.StartVideo()
+		})
+	})
+	drone.On(tello.VideoFrameEvent, func(data interface{}) {
+		pkt := data.([]byte)
+		if _, err := mplayerIn.Write(pkt); err != nil {
+			fmt.Println(err)
+		}
+	})
+}
+
+func fly(drone *tello.Driver) {
+	drone.TakeOff()
+	time.Sleep(time.Second * 10)
+	drone.Land()
 }
